508deb0b9fb09a0576a64753862fb15095abb96b
sam <sam@silver42.karoo.co.uk>
2015-08-12 08:50:18 +0100

    inlining

--- ludum/ScreenSwitcher.hx
+++ ludum/ScreenSwitcher.hx
@@ -5,7 +5,7 @@ import js.three.Vector3;
 
 // For switching between screens in the game
 class ScreenSwitcher {		
-	public static function checkForBoundaryCrossing(lastPosition:Vector3, currentPosition:Vector3):Bool {
+	public static inline function checkForBoundaryCrossing(lastPosition:Vector3, currentPosition:Vector3):Bool {
 		var lastNearestBoundary:Float = Math.round(lastPosition.x / Main.GAME_VIEWPORT_WIDTH) * Main.GAME_VIEWPORT_WIDTH + Main.GAME_VIEWPORT_WIDTH / 2;
 		var currentNearestBoundary:Float = Math.round(currentPosition.x / Main.GAME_VIEWPORT_WIDTH) * Main.GAME_VIEWPORT_WIDTH + Main.GAME_VIEWPORT_WIDTH / 2;
 		
@@ -16,11 +16,11 @@ class ScreenSwitcher {
 		return false;
 	}
 	
-	public static function getScreenIndices(position:Vector3):Vector2 {
+	public static inline function getScreenIndices(position:Vector3):Vector2 {
 		return new Vector2(Math.round(position.x / (Main.GAME_VIEWPORT_WIDTH)), Std.int(position.y / (Main.GAME_VIEWPORT_HEIGHT)));
 	}
 	
-	public static function getNearestCameraClampX(position:Vector3):Float {
+	public static inline function getNearestCameraClampX(position:Vector3):Float {
 		return Math.round(position.x / Main.GAME_VIEWPORT_WIDTH) * Main.GAME_VIEWPORT_WIDTH;
 	}
 }
